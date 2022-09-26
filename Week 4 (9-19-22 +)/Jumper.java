64450753572691bbe25c682c3fb0d03f97bcc910
Nathan Carpenter <ncarpenter@lshigh.org>
2022-09-26 11:39:57 -0700

    Expanded playing board

--- Week 4 (9-19-22 +)/Jumper.java
+++ Week 4 (9-19-22 +)/Jumper.java
@@ -3,26 +3,47 @@ import java.util.Scanner;
 public class Jumper {
     public static void main(String[] args){
         int starter = 1;
-        int upValue = 2;
-        int crossValue = 2;
+        int upValue = 3;
+        int crossValue = 3;
         String moveDir = "f";
 
         Scanner keyboard = new Scanner(System.in);
         System.out.println("You are currently in the exact middle (2,2)");
+        
 
         while(starter == 1){
 
+            // Defining Board
+
             String p1l1 = "|__";
             String p1l2 = "|__";
-            String p1l3 = "|__|";
+            String p1l3 = "|__";
+            String p1l4 = "|__";
+            String p1l5 = "|__";
 
             String p2l1 = "|__";
             String p2l2 = "|__";
-            String p2l3 = "|__|";
+            String p2l3 = "|__";
+            String p2l4 = "|__";
+            String p2l5 = "|__";
 
             String p3l1 = "|__";
             String p3l2 = "|__";
-            String p3l3 = "|__|";
+            String p3l3 = "|__";
+            String p3l4 = "|__";
+            String p3l5 = "|__";
+
+            String p4l1 = "|__";
+            String p4l2 = "|__";
+            String p4l3 = "|__";
+            String p4l4 = "|__";
+            String p4l5 = "|__";
+
+            String p5l1 = "|__";
+            String p5l2 = "|__";
+            String p5l3 = "|__";
+            String p5l4 = "|__";
+            String p5l5 = "|__";
 
      
             String picker = ("p"+crossValue+"l"+upValue);
@@ -37,7 +58,13 @@ public class Jumper {
                     p1l2 = "|X_";
                 }
                 else if (crossValue == 3){
-                    p1l3 = "|X_|";
+                    p1l3 = "|X_"; 
+                }
+                else if (crossValue == 4){
+                    p1l4 = "|X_"; 
+                }
+                else if (crossValue == 5){
+                    p1l5 = "|X_"; 
                 }
                 else{
                     System.out.println("crossValue error");
@@ -52,7 +79,13 @@ public class Jumper {
                     p2l2 = "|X_";
                 }
                 else if (crossValue == 3){
-                    p2l3 = "|X_|";
+                    p2l3 = "|X_";
+                }
+                else if (crossValue == 4){
+                    p2l4 = "|X_"; 
+                }
+                else if (crossValue == 5){
+                    p2l5 = "|X_"; 
                 }
                 else{
                     System.out.println("crossValue error");
@@ -67,27 +100,80 @@ public class Jumper {
                     p3l2 = "|X_";
                 }
                 else if (crossValue == 3){
-                    p3l3 = "|X_|";
+                    p3l3 = "|X_";
+                }
+                else if (crossValue == 4){
+                    p3l4 = "|X_"; 
+                }
+                else if (crossValue == 5){
+                    p3l5 = "|X_"; 
                 }
                 else{
                     System.out.println("crossValue error");
                 }
             }
+
+            else if (upValue == 4){
+                if(crossValue == 1){
+                    p4l1 = "|X_";
+                }
+                else if(crossValue == 2){
+                    p4l2 = "|X_";
+                }
+                else if (crossValue == 3){
+                    p4l3 = "|X_";
+                }
+                else if (crossValue == 4){
+                    p4l4 = "|X_"; 
+                }
+                else if (crossValue == 5){
+                    p4l5 = "|X_"; 
+                }
+                else{
+                    System.out.println("crossValue error");
+                }
+            }
+
+            else if (upValue == 5){
+                if(crossValue == 1){
+                    p5l1 = "|X_";
+                }
+                else if(crossValue == 2){
+                    p5l2 = "|X_";
+                }
+                else if (crossValue == 3){
+                    p5l3 = "|X_";
+                }
+                else if (crossValue == 4){
+                    p5l4 = "|X_"; 
+                }
+                else if (crossValue == 5){
+                    p5l5 = "|X_"; 
+                }
+                else{
+                    System.out.println("crossValue error");
+                }
+            }
+
+
+            
             else{
                 System.out.println("upValue Error");
             }
 
 
-            System.out.println("__________");
-            System.out.println(p3l1 + p3l2 + p3l3);
-            System.out.println(p2l1 + p2l2 + p2l3);
-            System.out.println(p1l1 + p1l2 + p1l3);
+            System.out.println("________________");
+            System.out.println(p5l1 + p5l2 + p5l3 + p5l4 + p5l5 +"|");
+            System.out.println(p4l1 + p4l2 + p4l3 + p4l4 + p4l5 +"|");
+            System.out.println(p3l1 + p3l2 + p3l3 + p3l4 + p3l5 +"|");
+            System.out.println(p2l1 + p2l2 + p2l3 + p2l4 + p2l5 + "|");
+            System.out.println(p1l1 + p1l2 + p1l3 + p1l4 + p1l5 + "|");
         
-            System.out.println("How would you like to move? (u,d,l,r)");
+            System.out.println("How would you like to move? (w,a,s,d)");
             moveDir = keyboard.nextLine();
 
-            if (moveDir.equals("u")){
-                if (upValue != 3){
+            if (moveDir.equals("w")){
+                if (upValue != 5){
                    upValue += 1; 
                 }
                 else{
@@ -96,7 +182,7 @@ public class Jumper {
 
             }
 
-            if (moveDir.equals("d")){
+            if (moveDir.equals("s")){
                 if (upValue != 1){
                     upValue -= 1; 
                  }
@@ -106,7 +192,7 @@ public class Jumper {
 
             }
 
-            if (moveDir.equals("l")){
+            if (moveDir.equals("a")){
                 if (crossValue != 1){
                     crossValue -= 1; 
                  }
@@ -116,8 +202,8 @@ public class Jumper {
 
             }
             
-            if(moveDir.equals("r")){
-                if (crossValue != 3){
+            if(moveDir.equals("d")){
+                if (crossValue != 5){
                     crossValue += 1; 
                 }                  
                 else{
---
+++ Week 4 (9-19-22 +)/Jumper2.java
@@ -0,0 +1,27 @@
+public class Jumper2 {
+    public static void main(String[] args){
+        Boolean[][] Board = {{false,false,false},{false,true,false},{false,false,false}};
+        
+
+
+        for(Boolean[] i: Board){
+            
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
