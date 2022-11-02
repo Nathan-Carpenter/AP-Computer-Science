b34ae74014b48a6fbe73bb0d6e1d9459339d3050
Nathan Carpenter <ncarpenter@lshigh.org>
2022-11-02 09:41:28 -0700

    work

--- Self Work/Jumper2.java
+++ Self Work/Jumper2.java
@@ -1,27 +1,62 @@
-public class Jumper2 {
-    public static void main(String[] args){
-        //Boolean[][] Board = {{false,false,false},{false,true,false},{false,false,false}};
-        
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+
+// import java.awt.Point;
+// import java.lang.reflect.Array;
+
+// import javax.swing.SwingUtilities;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+// Draw = outline
+// fill = filled in 
+
+// drawLine( start x, stary y, end x, end y);
+// setColor(Color.COLOR);
+// drawString("what you want to say", starting x, starting y)
+// drawRect(start x, start y, width, height)
+// drawArc(start x, start y, end x, end y)
+
+public class Jumper2 extends JPanel {
+    private static final long serialVersionUID = 7148504528835036003L;
 
 
-        //for(Boolean[] i: Board){
-            
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
 
+        int xValue = 10;
 
+        g.setColor(Color.BLACK);
+        g.fillRect(xValue, 500, 50, 50);
 
+        xValue ++;
 
+        repaint();
 
+       
+    }
 
+    public static void main(String[] args) {
 
-        //}
+        var panel = new Jumper2();
 
+        var frame = new JFrame("A simple graphics program");
+        frame.setSize(1920, 1080);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(panel, BorderLayout.CENTER);
+        frame.setVisible(true);
 
+    }
 
-
-
-
-
-
-
+    public static void TimeDelay(int x){
+        try{
+            Thread.sleep(x);
+        }
+        catch(InterruptedException ex){
+            ex.printStackTrace();
+        }
     }
 }
---
+++ Unit 2/Notes/graphingLines.java
@@ -0,0 +1,82 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.lang.reflect.Array;
+
+import javax.swing.SwingUtilities;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+// Draw = outline
+// fill = filled in 
+
+// drawLine( start x, stary y, end x, end y);
+// setColor(Color.COLOR);
+// drawString("what you want to say", starting x, starting y)
+// drawRect(start x, start y, width, height)
+// drawArc(start x, start y, end x, end y)
+
+public class graphingLines extends JPanel {
+    private static final long serialVersionUID = 7148504528835036003L;
+
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        int x = 0;
+        int y = 0;
+
+        g.setColor(Color.LIGHT_GRAY);
+
+        for (x = 0; x <= 500; x += 25){
+            g.drawLine(x,y,x,500);
+        }
+
+        for (y = 0; y <= 500; y += 25){
+            g.drawLine(0, y, 500, y);
+        }
+
+
+        g.setColor(Color.RED);
+
+        int prevX = 250;
+        int prevY = 250;
+
+        for(x = 0; x <= 500; x += 5){
+            y = (int) (250-(x*x)/20);
+            
+
+            // y -= xTRUE * xTRUE;
+
+         
+            //g.fillOval( x + 250 , y, 5, 5);
+            //g.fillOval( -x + 250, y, 5, 5);
+            g.drawLine(x + 250, y, prevX + 250, prevY  );
+            g.drawLine(-x + 250, y, -prevX + 250, prevY);
+            prevX = x;
+            prevY = y;
+            
+        }
+
+        
+        g.setColor(Color.BLACK);
+        g.drawLine(250, 0, 250, 500);
+        g.drawLine(0, 250, 500, 250);
+       
+   
+
+    }
+
+    public static void main(String[] args) {
+
+        var panel = new graphingLines();
+
+        var frame = new JFrame("Graph");
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(panel, BorderLayout.CENTER);
+        frame.setVisible(true);
+
+    }
+}
