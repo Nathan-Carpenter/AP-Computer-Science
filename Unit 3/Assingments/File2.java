563457879a164cbd800e65d7225ace22525b61fb
Nathan Carpenter <ncarpenter@lshigh.org>
2022-11-30 11:16:42 -0800

    completion

--- Unit 3/Assingments/File2.java
+++ Unit 3/Assingments/File2.java
@@ -4,8 +4,25 @@ import java.util.*;
 
 public class File2{
     public static void main(String[] args){
-        Function func = new Function(2);
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Please enter a number");
+        int var = Integer.parseInt(keyboard.nextLine());
+
+        MathFunctionOne math = new MathFunctionOne(var);
+        System.out.println(math.toString());
+
+        System.out.println("Please enter your first name ");
+        String name1 = keyboard.nextLine();
+
+        System.out.println("Please enter your last name ");
+        String name2 = keyboard.nextLine();
+
+        getInitials init = new getInitials(name1, name2);
+        System.out.println(init);
+
+        nameLength len = new nameLength(name1);
+        System.out.println(len);
         
-        System.out.println(func.toString());
     }
 }
