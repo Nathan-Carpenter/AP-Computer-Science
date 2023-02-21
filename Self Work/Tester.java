73bba509a6df627e69d46b2dcb7e4f2b13c8aa6f
Nathan Carpenter <ncarpenter@lshigh.org>
2023-02-21 12:20:43 -0800

    Unit 4 Start

--- Self Work/Tester.java
+++ Self Work/Tester.java
@@ -4,7 +4,31 @@
 
 public class Tester {
     public static void main(String[] args){
-        System.out.println();
+        String str = "CCCCCCCCAATTT!";
+        int currentLen = 0;
+        int maxLen = 0;
+        char preChar = ' ';
+        char streakChar = ' ';
+        int x = 0;
+
+        for(x = 0; x < str.length(); x++){
+            if(str.charAt(x) == preChar){
+                currentLen ++;
+            }
+            else{
+                currentLen = 1;
+                preChar = str.charAt(x);
+            }
+
+            if(currentLen > maxLen){
+                maxLen = currentLen;
+                streakChar = str.charAt(x);
+            }
+        }
+
+        System.out.println(streakChar + " " + maxLen);
+        
+
 
 
     }
