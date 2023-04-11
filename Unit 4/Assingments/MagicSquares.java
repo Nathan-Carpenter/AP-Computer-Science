5ebfb7031476049483a39629d816599c7b7e150d
Nathan Carpenter <ncarpenter@lshigh.org>
2023-04-11 13:07:55 -0700

    Magic Square Update

--- Unit 4/Assingments/MagicSquares.java
+++ Unit 4/Assingments/MagicSquares.java
@@ -1,6 +1,8 @@
+import java.util.*;
 public class MagicSquares {
     public static void main(String[] args){
         System.out.println("\033[H\033[2J");
+        Scanner k = new Scanner(System.in);
 
         System.out.println("3 x 3 Magic Square to test:");
         int[][] SquareTest = {{7,0,5},{2,4,6},{3,8,1}};
@@ -9,14 +11,78 @@ public class MagicSquares {
         System.out.println("4 x 4 Magic Square to test:");
         int[][] SquareTest2 = {{8,11,14,1},{13,2,7,13},{3,16,9,6},{10,5,4,15}};
         SquareChecker(SquareTest2);
-        
+
+        System.out.println("Please enter the size of the square: ");
+        int SquareSize = Integer.parseInt(k.nextLine());
+
+        System.out.println("Now randomly generating digits that make a magic square");
+        SquareRandom(SquareSize);
+
+
     }
+
+    public static void SquareRandom(int Size){
+
+        int[][] Square = new int[Size][Size];
+        boolean MagicTrue = true;
+        boolean MagicFinal = false;
+
+        while(MagicFinal){
+
+            for(int y = 0; y < Size; y ++){
+                for(int x = 0; x < Size; x++){
+                    Square[y][x] = (int)(Math.random() * Size);
+                }
+            }
+
+            int finder = 0;
+            for(int i = 0; i < Square[0].length; i++){
+                finder += Square[0][i];
+            }
+            MagicTrue = true;
+            int sum = 0;
+    
+            for(int y = 0; y < Square[0].length; y++){
+                sum = 0;
+                for(int x = 0; x < Square[0].length; x++){
+                    sum += Square[y][x];
+                }
+                if (sum == finder && MagicTrue == true){
+                    MagicTrue = true;
+                }
+                else{
+                    MagicTrue = false;
+                }
+            }
+            for(int x = 0; x < Square[0].length; x++){
+                sum = 0;
+                for(int y = 0; y < Square[0].length; y++){
+                    sum += Square[y][x];
+                }
+                if (sum == finder && MagicTrue == true){
+                    MagicTrue = true;
+                }
+                else{
+                    MagicTrue = false;
+                }
+    
+            }
+            MagicFinal = MagicTrue;
+        }
+
+        SquareChecker(Square);
+    }
+
+    
 
     public static boolean SquareChecker(int[][] Square){
         System.out.println(" ");
         System.out.println("==========");
         System.out.println(" ");
-        int finder = Square[0][0] + Square[0][1] + Square[0][2];
+        int finder = 0;
+        for(int i = 0; i < Square[0].length; i++){
+            finder += Square[0][i];
+        }
         boolean MagicTrue = true;
         int sum = 0;
    
@@ -33,7 +99,7 @@ public class MagicSquares {
                 }
 
             }
-            System.out.println(" = " + sum);
+            System.out.println("= " + sum);
 
             if (sum == finder && MagicTrue == true){
                 MagicTrue = true;
@@ -53,7 +119,7 @@ public class MagicSquares {
             for(int y = 0; y < Square[0].length; y++){
                 sum += Square[y][x];
             }
-            System.out.print(sum + " ");
+            System.out.print(sum + "  ");
 
             if (sum == finder && MagicTrue == true){
                 MagicTrue = true;
