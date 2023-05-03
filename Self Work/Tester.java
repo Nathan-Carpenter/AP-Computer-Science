ed3f3664656b643ee8307b1eca2773b58e143d61
Nathan Carpenter <ncarpenter@lshigh.org>
2023-05-02 21:32:27 -0700

    e

--- Self Work/Tester.java
+++ Self Work/Tester.java
@@ -4,7 +4,13 @@
 
 public class Tester {
     public static void main(String[] args){
-        System.out.println();
+        String oldSeq = "00000111";
+        String segment = "11";
+
+        String newSeq = oldSeq.replaceFirst(segment, "");
+        System.out.println(newSeq);
+
+        System.out.print(Math.pow(5.0, 2));
 
 
     }
